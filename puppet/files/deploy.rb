17c31b6db64137636244a5b05e4844583741f2d3
Puneet Goyal <pungoyal@gmail.com>
2011-05-18 20:30:24 +1000

    changes

--- puppet/files/deploy.rb
+++ puppet/files/deploy.rb
@@ -1,17 +1,19 @@
 module MCollective
   module Agent
     class Deploy < RPC::Agent
-      metadata :name        => "Deploy",
+      metadata :name => "Deploy",
                :description => "Deployment agent",
-               :author      => "Anonymous",
-               :version     => "0.1",
-               :license     => "private",
-               :url         => "http://cn.com",
-               :timeout     => 600
+               :author => "Anonymous",
+               :version => "0.1",
+               :license => "private",
+               :url => "http://cn.com",
+               :timeout => 600
 
       action "war" do
         validate :source, String
-
+        p '*'*10
+        p request[:source]
+        p '*'*10
       end
     end
   end
--- puppet/manifests/app.pp
+++ puppet/manifests/app.pp
@@ -5,3 +5,22 @@ class app inherits base {
 }
 
 include app
+
+file { "/var/db/prevaylor":
+    ensure  => "directory"
+}
+
+package { "tomcat6":
+    ensure   => "present",
+}
+
+file { "/etc/tomcat6/tomcat6.conf":
+    ensure  => "present",
+    source  => "file://${files_dir}/tomcat6.conf",
+    require => Package["tomcat6"]
+}
+
+service {
+    tomcat6: ensure => running,
+    require => File["/etc/tomcat6/tomcat6.conf"]
+}
